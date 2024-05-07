448ba3f3032f02deb42f012c639ccad1c658681e
Nilabja Roy <nilabjaroy2001@gmail.com>
2024-05-07 21:41:38 +0530

    pups

---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,IntegerField
+
+
+class AddForm(FlaskForm):
+
+    name=StringField('Name of Puppy: ')
+    submit=SubmitField('Add Puppy')
+
+
+class DelForm(FlaskForm):
+
+    id=IntegerField('Id Number of Puppy to Remove: ')
+    submit=SubmitField('Remove Puppy')
